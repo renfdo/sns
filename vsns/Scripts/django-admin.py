57fd06abd96190a86c8b47df96c5ed6dd5e33928
Renan <renan.feitosa@resourceit.com>
2020-04-29 17:29:42 -0300

    add ao

--- vsns/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\re036935\documents\gitambev\ambev-sns\vsns\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
